af1d6c5c68dbfba7f85a95f5a65d466f3a5defda
郭雪健 <guoxj18@lzu.edu.cn>
2022-03-12 11:48:41 +0800

    2022-03-12

---
+++ 590.n-叉树的后序遍历.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=590 lang=golang
+ *
+ * [590] N 叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func postorder(root *Node) (res []int) {
+    var dfs func(node *Node)
+	dfs = func(node *Node) {
+		if node == nil {
+			return
+		}
+		for _, child := range node.Children {
+			dfs(child)
+		}
+		res = append(res, node.Val)
+	}
+	dfs(root)
+	return
+}
+// @lc code=end
+
---
+++ 590.n-叉树的后序遍历.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=590 lang=python3
+#
+# [590] N 叉树的后序遍历
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res = []
+        def dfs(node):
+            if not node:
+                return
+            for child in node.children:
+                dfs(child)
+            res.append(node.val)
+        
+        dfs(root)
+        return res
+# @lc code=end
+
