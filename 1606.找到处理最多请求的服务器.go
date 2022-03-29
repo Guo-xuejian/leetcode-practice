24a175a8dfcd395e73e5502434bb7fb6b9748289
郭雪健 <guoxj18@lzu.edu.cn>
2022-03-30 00:11:12 +0800

    2022-03-30

---
+++ 1606.找到处理最多请求的服务器.go
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=1606 lang=golang
+ *
+ * [1606] 找到处理最多请求的服务器
+ */
+
+// @lc code=start
+func busiestServers(k int, arrival, load []int) (ans []int) {
+	available := redblacktree.NewWithIntComparator()
+	for i := 0; i < k; i++ {
+		available.Put(i, nil)
+	}
+	busy := hp{}
+	requests := make([]int, k)
+	maxRequest := 0
+	for i, t := range arrival {
+		for len(busy) > 0 && busy[0].end <= t {
+			available.Put(busy[0].id, nil)
+			heap.Pop(&busy)
+		}
+		if available.Size() == 0 {
+			continue
+		}
+		node, _ := available.Ceiling(i % k)
+		if node == nil {
+			node = available.Left()
+		}
+		id := node.Key.(int)
+		requests[id]++
+		if requests[id] > maxRequest {
+			maxRequest = requests[id]
+			ans = []int{id}
+		} else if requests[id] == maxRequest {
+			ans = append(ans, id)
+		}
+		heap.Push(&busy, pair{t + load[i], id})
+		available.Remove(id)
+	}
+	return
+}
+
+type pair struct{ end, id int }
+type hp []pair
+
+func (h hp) Len() int            { return len(h) }
+func (h hp) Less(i, j int) bool  { return h[i].end < h[j].end }
+func (h hp) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *hp) Push(v interface{}) { *h = append(*h, v.(pair)) }
+func (h *hp) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+
+// @lc code=end
+
