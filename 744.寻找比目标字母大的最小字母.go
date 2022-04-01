dfda316fb7b2f703d60b61c52e826ea1fde23b09
郭雪健 <guoxj18@lzu.edu.cn>
2022-04-02 01:06:33 +0800

    2022-04-02+

---
+++ 744.寻找比目标字母大的最小字母.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=744 lang=golang
+ *
+ * [744] 寻找比目标字母大的最小字母
+ */
+
+// @lc code=start
+func nextGreatestLetter(letters []byte, target byte) byte {
+	for _, letter := range letters {
+		if letter > target {
+			return letter
+		}
+	}
+	return letters[0]
+}
+
+// @lc code=end
+
---
+++ 744.寻找比目标字母大的最小字母.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=744 lang=python3
+#
+# [744] 寻找比目标字母大的最小字母
+#
+
+# @lc code=start
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for letter in letters:
+            if letter > target:
+                return letter
+        return letters[0]
+# @lc code=end
+
