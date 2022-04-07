38b65309de7e7315e57aa1b1ea1f14b95df5295c
郭雪健 <guoxj18@lzu.edu.cn>
2022-04-08 00:43:01 +0800

    2022-04-08-plan-complete

---
+++ 350.两个数组的交集-ii.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=350 lang=python3
+#
+# [350] 两个数组的交集 II
+#
+
+# @lc code=start
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            return self.intersect(nums2, nums1)
+        
+        m = collections.Counter()
+        for num in nums1:
+            m[num] += 1
+        
+        intersection = list()
+        for num in nums2:
+            if (count := m.get(num, 0)) > 0:
+                intersection.append(num)
+                m[num] -= 1
+                if m[num] == 0:
+                    m.pop(num)
+        
+        return intersection
+# @lc code=end
+
---
+++ 633.平方数之和.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=633 lang=golang
+ *
+ * [633] 平方数之和
+ */
+
+// @lc code=start
+func judgeSquareSum(c int) bool {
+	left, right := 0, int(math.Sqrt(float64(c)))
+	for left <= right {
+		sum := left*left + right*right
+		if sum == c {
+			return true
+		} else if sum > c {
+			right--
+		} else {
+			left++
+		}
+	}
+	return false
+}
+
+// @lc code=end
+
