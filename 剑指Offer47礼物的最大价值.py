8d457b0361fe311de02e8b79fc40fc9a39e76baa
郭雪健 <guoxj18@lzu.edu.cn>
2022-04-20 00:42:32 +0800

    plan-complete

--- 剑指Offer47礼物的最大价值.py
+++ 剑指Offer47礼物的最大价值.py
@@ -1,3 +1,23 @@
+# 剑指 Offer 47. 礼物的最大价值
+# 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+
+# 示例 1:
+
+# 输入:
+# [
+#   [1,3,1],
+#   [1,5,1],
+#   [4,2,1]
+# ]
+# 输出: 12
+# 解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+
+# 提示：
+
+# 0 < grid.length <= 200
+# 0 < grid[0].length <= 200
+
+
 class Solution:
     def maxValue(self, grid: List[List[int]]) -> int:
         for i in range(len(grid)):
@@ -15,3 +35,16 @@ class Solution:
                 else:
                     grid[i][j] += max(grid[i - 1][j], grid[i][j - 1])
         return grid[-1][-1]
+
+# 2022-04-20
+# class Solution:
+#     def maxValue(self, grid: List[List[int]]) -> int:
+#         m, n = len(grid), len(grid[0])
+#         for i in range(1, n):
+#             grid[0][i] += grid[0][i - 1]
+#         for j in range(1, m):
+#             grid[j][0] += grid[j - 1][0]
+#         for i in range(1, m):
+#             for j in range(1, n):
+#                 grid[i][j] += max(grid[i - 1][j], grid[i][j - 1])
+#         return grid[m - 1][n - 1]
