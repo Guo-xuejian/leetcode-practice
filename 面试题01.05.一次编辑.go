17593ddcc9027c0b012737b06a7bd5ad0313199d
郭雪健 <guoxj18@lzu.edu.cn>
2022-05-13 00:39:23 +0800

    2022-02-13

---
+++ 面试题01.05.一次编辑.go
@@ -0,0 +1,35 @@
+// 面试题 01.05. 一次编辑
+// 字符串有三种编辑操作:插入一个字符、删除一个字符或者替换一个字符。 给定两个字符串，编写一个函数判定它们是否只需要一次(或者零次)编辑。
+
+// 示例 1:
+
+// 输入:
+// first = "pale"
+// second = "ple"
+// 输出: True
+
+// 示例 2:
+
+// 输入:
+// first = "pales"
+// second = "pal"
+// 输出: False
+
+func oneEditAway(first, second string) bool {
+	m, n := len(first), len(second)
+	if m < n {
+		return oneEditAway(second, first)
+	}
+	if m-n > 1 {
+		return false
+	}
+	for i, ch := range second {
+		if first[i] != byte(ch) {
+			if m == n {
+				return first[i+1:] == second[i+1:]
+			}
+			return first[i+1:] == second[i:]
+		}
+	}
+	return true
+}
