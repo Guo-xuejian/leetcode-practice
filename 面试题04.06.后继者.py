0df16f822302053362cc41c16d2b05fc8d640197
郭雪健 <guoxj18@lzu.edu.cn>
2022-05-16 00:29:38 +0800

    2022-05-16 plus

--- 面试题04.06.后继者.py
+++ 面试题04.06.后继者.py
@@ -29,11 +29,11 @@
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> TreeNode:
