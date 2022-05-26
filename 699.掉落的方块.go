847300cd7d04d369538f9aee89cb109914647976
郭雪健 <guoxj18@lzu.edu.cn>
2022-05-26 14:33:49 +0800

    2022-05-26

---
+++ 699.掉落的方块.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=699 lang=golang
+ *
+ * [699] 掉落的方块
+ */
+
+// @lc code=start
+func fallingSquares(positions [][]int) []int {
+
+}
+// @lc code=end
+
---
+++ 699.掉落的方块.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=699 lang=python3
+#
+# [699] 掉落的方块
+#
+
+# @lc code=start
+class Solution:
+    def fallingSquares(self, positions: List[List[int]]) -> List[int]:
+        n = len(positions)
+        heights = [0] * n
+        for i, (left1, side1) in enumerate(positions):
+            right1 = left1 + side1 - 1
+            heights[i] = side1
+            for j in range(i):
+                left2, right2 = positions[j][0], positions[j][0] + positions[j][1] - 1
+                if right1 >= left2 and right2 >= left1:
+                    heights[i] = max(heights[i], heights[j] + side1)
+        for i in range(1, n):
+            heights[i] = max(heights[i], heights[i - 1])
+        return heights + [1]
+# @lc code=end
+
