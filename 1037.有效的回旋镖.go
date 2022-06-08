a5d4b2f6ebc94aab0ba647d1c69b1244b87e3ca7
郭雪健 <guoxj18@lzu.edu.cn>
2022-06-08 09:34:07 +0800

    2022-06-08

---
+++ 1037.有效的回旋镖.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1037 lang=golang
+ *
+ * [1037] 有效的回旋镖
+ */
+
+// @lc code=start
+func isBoomerang(points [][]int) bool {
+    v1 := [2]int{points[1][0] - points[0][0], points[1][1] - points[0][1]}
+    v2 := [2]int{points[2][0] - points[0][0], points[2][1] - points[0][1]}
+    return v1[0]*v2[1]-v1[1]*v2[0] != 0
+}
+// @lc code=end
+
