7d2d750d571498102e750393793dadfda461409b
郭雪健 <guoxj18@lzu.edu.cn>
2022-06-17 00:25:48 +0800

    2022-06-17

---
+++ 1089.复写零.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=1089 lang=golang
+ *
+ * [1089] 复写零
+ */
+
+// @lc code=start
+func duplicateZeros(arr []int)  {
+
+}
+// @lc code=end
+
---
+++ 1089.复写零.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode.cn id=1089 lang=python3
+#
+# [1089] 复写零
+#
+
+# @lc code=start
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        n = len(arr)
+        top = 0
+        i = -1
+        while top < n:
+            i += 1
+            top += 1 if arr[i] else 2
+        j = n - 1
+        if top == n + 1:
+            arr[j] = 0
+            j -= 1
+            i -= 1
+        while j >= 0:
+            arr[j] = arr[i]
+            j -= 1
+            if arr[i] == 0:
+                arr[j] = arr[i]
+                j -= 1
+            i -= 1
+# @lc code=end
+
