1bd755556fb9c9b8be78bc90e3ca2f0dd5902e60
郭雪健 <guoxj18@lzu.edu.cn>
2022-06-18 19:05:18 +0800

    2022-06-189

--- 1089.复写零.go
+++ 1089.复写零.go
@@ -5,8 +5,34 @@
  */
 
 // @lc code=start
-func duplicateZeros(arr []int)  {
-
+func duplicateZeros(arr []int) {
+	n := len(arr)
+	top := 0
+	i := -1
+	for top < n {
+		i++
+		if arr[i] != 0 {
+			top++
+		} else {
+			top += 2
+		}
+	}
+	j := n - 1
+	if top == n+1 {
+		arr[j] = 0
+		j--
+		i--
+	}
+	for j >= 0 {
+		arr[j] = arr[i]
+		j--
+		if arr[i] == 0 {
+			arr[j] = arr[i]
+			j--
+		}
+		i--
+	}
 }
+
 // @lc code=end
 
