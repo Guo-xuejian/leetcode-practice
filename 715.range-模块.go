8d50f364bb9174285266c95393dc3898c0d4e3b9
郭雪健 <guoxj18@lzu.edu.cn>
2022-06-20 11:49:55 +0800

    2022-06-20

---
+++ 715.range-模块.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=715 lang=golang
+ *
+ * [715] Range 模块
+ */
+
+// @lc code=start
+type RangeModule struct {
+
+}
+
+
+func Constructor() RangeModule {
+
+}
+
+
+func (this *RangeModule) AddRange(left int, right int)  {
+
+}
+
+
+func (this *RangeModule) QueryRange(left int, right int) bool {
+
+}
+
+
+func (this *RangeModule) RemoveRange(left int, right int)  {
+
+}
+
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddRange(left,right);
+ * param_2 := obj.QueryRange(left,right);
+ * obj.RemoveRange(left,right);
+ */
+// @lc code=end
+
