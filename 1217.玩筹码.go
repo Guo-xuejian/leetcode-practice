8d61deb8de7a2d4338bcddfd9284000d69b6dc21
郭雪健 <guoxj18@lzu.edu.cn>
2022-07-08 00:40:51 +0800

    2022-07-08

--- 1217.玩筹码.go
+++ 1217.玩筹码.go
@@ -22,5 +22,21 @@ func min(x, y int) int {
 	return x
 }
 
+
+// func minCostToMoveChips(position []int) int {
+//     cnt := [2]int{}
+//     for _, p := range position {
+//         cnt[p%2]++
+//     }
+//     return min(cnt[0], cnt[1])
+// }
+
+// func min(a, b int) int {
+//     if a > b {
+//         return b
+//     }
+//     return a
+// }
+
 // @lc code=end
 
--- 1217.玩筹码.py
+++ 1217.玩筹码.py
@@ -11,5 +11,11 @@ class Solution:
         # 所以实际上偶数位上的点可以看做同一个点，奇数位同样，比较奇偶数那个小一些，移动的代价就最小
         odd_num = sum(pos % 2 for pos in position)
         return min(odd_num, len(position) - odd_num)
+
+
+# class Solution:
+#     def minCostToMoveChips(self, position: List[int]) -> int:
+#         cnt = Counter(p % 2 for p in position)  # 根据模 2 后的余数来统计奇偶个数
+#         return min(cnt[0], cnt[1])
 # @lc code=end
 
