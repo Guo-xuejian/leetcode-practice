b35dc200ec246197e8dc449f62b6428a172f3a71
郭雪健 <guoxj18@lzu.edu.cn>
2022-07-12 00:24:20 +0800

    2022-07-12

--- 1252.奇数值单元格的数目.go
+++ 1252.奇数值单元格的数目.go
@@ -29,5 +29,27 @@ func oddCells(m int, n int, indices [][]int) (res int) {
 	return
 }
 
+// 20223-07-12
+// func oddCells(m, n int, indices [][]int) (ans int) {
+//     matrix := make([][]int, m)
+//     for i := range matrix {
+//         matrix[i] = make([]int, n)
+//     }
+//     for _, p := range indices {
+//         for j := range matrix[p[0]] {
+//             matrix[p[0]][j]++
+//         }
+//         for _, row := range matrix {
+//             row[p[1]]++
+//         }
+//     }
+//     for _, row := range matrix {
+//         for _, v := range row {
+//             ans += v % 2
+//         }
+//     }
+//     return
+// }
+
 // @lc code=end
 
--- 1252.奇数值单元格的数目.py
+++ 1252.奇数值单元格的数目.py
@@ -14,5 +14,16 @@ class Solution:
             for i in range(n):
                 matrix[x][i] += 1
         return sum(elem % 2 == 1 for line in matrix for elem in line)
+
+# 2022-07-12
+# class Solution:
+#     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+#         matrix = [[0] * n for _ in range(m)]
+#         for x, y in indices:
+#             for j in range(n):
+#                 matrix[x][j] += 1
+#             for row in matrix:
+#                 row[y] += 1
+#         return sum(x % 2 for row in matrix for x in row)
 # @lc code=end
 
