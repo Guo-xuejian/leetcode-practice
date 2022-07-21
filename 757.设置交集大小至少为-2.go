1900b2d8623fb81cde6a504d40f594ced51f12c4
郭雪健 <guoxj18@lzu.edu.cn>
2022-07-22 00:14:46 +0800

    2022-07-22

---
+++ 757.设置交集大小至少为-2.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=757 lang=golang
+ *
+ * [757] 设置交集大小至少为2
+ */
+
+// @lc code=start
+func intersectionSizeTwo(intervals [][]int) (ans int) {
+    sort.Slice(intervals, func(i, j int) bool {
+        a, b := intervals[i], intervals[j]
+        return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+    })
+    n, m := len(intervals), 2
+    vals := make([][]int, n)
+    for i := n - 1; i >= 0; i-- {
+        for j, k := intervals[i][0], len(vals[i]); k < m; k++ {
+            ans++
+            for p := i - 1; p >= 0 && intervals[p][1] >= j; p-- {
+                vals[p] = append(vals[p], j)
+            }
+            j++
+        }
+    }
+    return
+}
+// @lc code=end
+
