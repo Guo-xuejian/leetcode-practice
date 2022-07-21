66471540215e5fb1f9c84e14e09a89965bb97f5c
郭雪健 <guoxj18@lzu.edu.cn>
2022-07-21 23:21:45 +0800

    2022-07-21

--- 1260.二维网格迁移.go
+++ 1260.二维网格迁移.go
@@ -6,18 +6,19 @@
 
 // @lc code=start
 func shiftGrid(grid [][]int, k int) [][]int {
-    m, n := len(grid), len(grid[0])
-    ans := make([][]int, m)
-    for i := range ans {
-        ans[i] = make([]int, n)
-    }
-    for i, row := range grid {
-        for j, v := range row {
-            index1 := (i*n + j + k) % (m * n)
-            ans[index1/n][index1%n] = v
-        }
-    }
-    return ans
+	m, n := len(grid), len(grid[0])
+	ans := make([][]int, m)
+	for i := range ans {
+		ans[i] = make([]int, n)
+	}
+	for i, row := range grid {
+		for j, v := range row {
+			index1 := (i*n + j + k) % (m * n)
+			ans[index1/n][index1%n] = v
+		}
+	}
+	return ans
 }
+
 // @lc code=end
 
---
+++ 814.二叉树剪枝.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=814 lang=python3
+#
+# [814] 二叉树剪枝
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if root.left is None and root.right is None and root.val == 0:
+            return None
+        return root
+# @lc code=end
+
